bd38c5bd35ac67285765be7990047e5384a2b5db
k09at1ch <117813113+k09at1ch@users.noreply.github.com>
2023-05-02 15:50:22 +0300

    ПЕРЕРОБКА

--- src/fetchcountries.js
+++ src/fetchcountries.js
@@ -1,17 +1,16 @@
 export function fetchCountries(name) {
   const url =
-    'https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages';
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
   return fetch(url)
     .then(response => {
       if (response.status === 404) {
-        throw new Error('Country not found');
+        Notiflix.Notify.warning('Country not found');
+        return Promise.reject('Country not found');
       }
       return response.json();
     })
     .then(data => {
-      if (data.length >= 10) {
-        throw new Error('Too many countries found');
-      }
       return data;
     });
 }
+
